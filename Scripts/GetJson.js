417f5a6534ee312fd4d3eabea9f8135ee9f9dc70
Jakub Panczyszyn <zatroxkp@gmail.com>
2020-06-19 09:48:23 +0100

    Tidy up UI

--- Scripts/GetJson.js
+++ Scripts/GetJson.js
@@ -9,9 +9,20 @@ function GetPetitionDetails() {
     }
 
     var petitionURL = "https://petition.parliament.uk/petitions/" + petitionId + ".json";
-    $.getJSON(petitionURL, function(data) {ProcessData(data)});
+    $.getJSON(petitionURL, function(data) { ProcessData(data) });
 }
 
 function ProcessData(data) {
     debugger;
+    var link, title, background, moreDetails, signatureCount,
+        topics, departments;
+
+    link = data.links.self;
+    title = data.data.attributes.action;
+    background = data.data.attributes.background;
+    moreDetails = data.data.attributes.additional_details;
+    signatureCount = data.data.attributes.signature_count;
+    topics = data.data.attributes.topics;
+    departments = data.data.attributes.departments;
+
 }
