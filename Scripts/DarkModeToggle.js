f4b01754e36e1c526fdc4f58c8801450c1690c51
Jakub Panczyszyn <zatroxkp@gmail.com>
2020-07-28 17:47:41 +0100

    Dark Mode Improvements
    changes the green signature count text and performs a darkMode check when document is ready

--- Scripts/DarkModeToggle.js
+++ Scripts/DarkModeToggle.js
@@ -1,12 +1,20 @@
+$(document).ready(function() {
+    debugger;
+    ToggleDarkMode();
+})
+
 function ToggleDarkMode() {
     debugger;
     var toggleSwitch = document.getElementById("darkMode");
+    var signatureCount = document.getElementById("signature-count");
 
     if (toggleSwitch.checked == true) {
         document.body.style.backgroundColor = "grey";
         document.body.style.color = "white";
+        signatureCount.style.color = "#00cc00";
     } else {
         document.body.style.backgroundColor = "white";
         document.body.style.color = "black";
+        signatureCount.style.color = "#008800";
     }
 }
