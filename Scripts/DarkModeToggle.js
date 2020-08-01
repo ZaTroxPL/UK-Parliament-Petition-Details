c2a1511583d7a04be2af572bd1f21734c5e590d1
Jakub Panczyszyn <zatroxkp@gmail.com>
2020-08-01 08:36:00 +0100

    Changed Departments styling
    Made it more obvious that you can clickon "Departments" to show/hide departments table

--- Scripts/DarkModeToggle.js
+++ Scripts/DarkModeToggle.js
@@ -7,11 +7,13 @@ function ToggleDarkMode() {
         document.body.style.backgroundColor = "grey";
         document.body.style.color = "white";
         signatureCount.style.color = "#00cc00";
+        $("#departments-div>span").css("border", "white 2px solid");
         $("#departments-table>thead>tr>th").css("border-bottom", "white 1px solid");
     } else {
         document.body.style.backgroundColor = "white";
         document.body.style.color = "black";
         signatureCount.style.color = "#008800";
+        $("#departments-div>span").css("border", "black 2px solid");
         $("#departments-table>thead>tr>th").css("border-bottom", "black 1px solid");
     }
 }
